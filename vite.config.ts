59491b4cf4db7d22f99792938cdc63da2e3e3ddb
Luis Bravo <luismanuelbravocazares@hotmail.com>
2025-08-12 15:50:25 -0700

    gh configurado

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repo = 'utilidades';
+const user = 'usuario';
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repo}/`,
 })
