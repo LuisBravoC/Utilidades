cfbfa984eb7fe6bf72e37d29b464b19eed31a38c
Luis Bravo <luismanuelbravocazares@hotmail.com>
2025-08-12 15:54:45 -0700

    gh pages añadido

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const repo = 'utilidades';
-const user = 'usuario';
 
 export default defineConfig({
   plugins: [react()],
